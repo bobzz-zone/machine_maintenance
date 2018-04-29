48b4d13ca126bd9bc4f81f0c70bfa7c4aa618505
Bobby <bobzz.zone@gmail.com>
2018-04-29 19:25:17 +0000

    aa

--- machine_maintenance/machine_maintenance/doctype/maintenance_plan/maintenance_plan.js
+++ machine_maintenance/machine_maintenance/doctype/maintenance_plan/maintenance_plan.js
@@ -3,6 +3,15 @@
 
 frappe.ui.form.on('Maintenance Plan', {
 	refresh: function(frm) {
+		// cur_frm.add_fetch(link_field, source_fieldname, target_fieldname);
+cur_frm.add_fetch("maintenance_sop", "type", "type");
+cur_frm.add_fetch("maintenance_sop", "work_type", "work_type");
+cur_frm.add_fetch("maintenance_sop", "level", "level");
+cur_frm.add_fetch("maintenance_sop", "duration", "duration");
+cur_frm.add_fetch("maintenance_sop", "sop", "sop");
 
+cur_frm.add_fetch("maintenance_frequency", "lead_time", "lead_time");
+cur_frm.add_fetch("maintenance_frequency", "frequency", "frequency");
+cur_frm.add_fetch("maintenance_frequency", "reschedule", "reschedule");
 	}
 });
