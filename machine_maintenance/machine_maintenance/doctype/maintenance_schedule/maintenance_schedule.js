2971cb3fbcc13c88e1aa3c47573881c3d6b64115
Bobby <bobzz.zone@gmail.com>
2018-04-29 19:28:49 +0000

    aa

---
+++ machine_maintenance/machine_maintenance/doctype/maintenance_schedule/maintenance_schedule.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2018, Bobby and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Maintenance Schedule', {
+	refresh: function(frm) {
+// cur_frm.add_fetch(link_field, source_fieldname, target_fieldname);
+cur_frm.add_fetch("maintenance_sop", "type", "type");
+cur_frm.add_fetch("maintenance_sop", "work_type", "work_type");
+cur_frm.add_fetch("maintenance_sop", "level", "level");
+cur_frm.add_fetch("maintenance_sop", "duration", "duration");
+cur_frm.add_fetch("maintenance_sop", "sop", "sop");
+
+	}
+});
